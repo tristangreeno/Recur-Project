da2183e0afbe28fe2a41a6dab08b8591f2bb3df3
tristangreeno <greeno.tristan@gmail.com>
2016-06-16 14:28:52 -0500

    make sendEmail standalone class

--- app/controllers/EmailController.scala
+++ app/controllers/EmailController.scala
@@ -2,12 +2,12 @@ package controllers
 
 import javax.inject.Inject
 
-import be.objectify.deadbolt.scala.ActionBuilders
+import models._
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.libs.mailer._
 import play.api.mvc._
 import repos.{SubscriptionsRepo, UsersRepo}
-import security.AuthSupport
+import slick.lifted.TableQuery
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent._
@@ -16,24 +16,25 @@ import scala.concurrent.duration._
 /**
   * Created by tristangreeno on 6/16/16.
   */
-class EmailController @Inject()(application: Application, mailerClient: MailerClient, actionBuilder: ActionBuilders, authSupport: AuthSupport, usersRepo: UsersRepo, subscriptionsRepo: SubscriptionsRepo,
+class EmailController @Inject()(application: Application, mailerClient: MailerClient, usersRepo: UsersRepo, subscriptionsRepo: SubscriptionsRepo,
                                  val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
-  def sendEmail = actionBuilder.SubjectPresentAction().defaultHandler() { authRequest =>
-    authSupport.currentUser(authRequest).map(maybeUser => {
+  def sendEmail = Action { implicit request =>
 
-      val user = application.getCurrentUser(maybeUser.get.userId)
-      val userId = user.get.id.get
-      val allList = Await.result(subscriptionsRepo.list(userId).map(list => list), 10.seconds)
-      val renewList = Await.result(subscriptionsRepo.listSubsAboutToRenew(userId).map(list => list), 10.seconds)
+    val users = Await.result(usersRepo.options(), 10.seconds)
 
-      val email = Email(
-        "Reminder: Subscription about to Renew",
-        "Recur Application <recur.application@gmail.com>",
-        Seq(s"${user.get.name} TO <${user.get.name}>"),
-        bodyText = Some(s"Your subscriptions will be renewing soon."),
-        bodyHtml = Some(
-          s"""
+    for (user <- users) yield {
+        val userInList = User.apply(Some(user._1), user._2, user._3, user._4)
+        val userId = userInList.id.get
+        val renewList = Await.result(subscriptionsRepo.listSubsAboutToRenew(userId).map(list => list), 10.seconds)
+
+        val email = Email(
+      "Reminder: Subscription about to Renew",
+      "Recur Application <recur.application@gmail.com>",
+      Seq(s"${userInList.name} TO <${userInList.name}>"),
+      bodyText = Some(s"Your subscriptions will be renewing soon."),
+      bodyHtml = Some(
+        s"""
       <html lang="en">
       <body>
         <b> The following subscriptions will be renewing soon</b>
@@ -42,11 +43,12 @@ class EmailController @Inject()(application: Application, mailerClient: MailerCl
         <a href="https://glacial-brook-71360.herokuapp.com/subscriptions">Subscriptions List</a>
       </body>
       </html>""")
-      )
+    )
 
-      mailerClient.send(email)
+    mailerClient.send(email)}
 
-      Ok
-    })}
+    Ok
 
+
+  }
 }
