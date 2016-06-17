f7e86164f78779e2e29d8bf3be63841953d0bccf
tristangreeno <greeno.tristan@gmail.com>
2016-06-17 15:19:54 -0500

    select2

--- target/scala-2.11/twirl/main/views/html/noUser.template.scala
+++ target/scala-2.11/twirl/main/views/html/noUser.template.scala
@@ -31,18 +31,20 @@ Seq[Any](format.raw/*1.17*/("""
         <title>Recur</title>
         <link rel="stylesheet" href=""""),_display_(/*8.39*/routes/*8.45*/.Assets.versioned("stylesheets/bootstrap.min.css")),format.raw/*8.95*/("""">
         <script src=""""),_display_(/*9.23*/routes/*9.29*/.Assets.versioned("javascripts/bootstrap.min.js")),format.raw/*9.78*/("""" type="text/javascript"></script>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
     </head>
     <header class="topbar">
         <h1 class="fill">
-            <a href=""""),_display_(/*13.23*/routes/*13.29*/.Application.index()),format.raw/*13.49*/("""">
+            <a href=""""),_display_(/*15.23*/routes/*15.29*/.Application.index()),format.raw/*15.49*/("""">
         Recur &mdash; Subscriptions Database
       </a>
         </h1>
     </header>
 
     <section id="main">
-    """),_display_(/*20.6*/content),format.raw/*20.13*/("""
-    """),format.raw/*21.5*/("""</section>
+    """),_display_(/*22.6*/content),format.raw/*22.13*/("""
+    """),format.raw/*23.5*/("""</section>
 </html>"""))
       }
     }
@@ -63,11 +65,11 @@ Seq[Any](format.raw/*1.17*/("""
 object noUser extends noUser_Scope0.noUser
               /*
                   -- GENERATED --
-                  DATE: Thu Jun 16 18:16:17 CDT 2016
+                  DATE: Fri Jun 17 15:00:02 CDT 2016
                   SOURCE: /Users/tristangreeno/workspace/PlayProject/app/views/noUser.scala.html
-                  HASH: 7c42b47039082d2c15ebc4914bc436597eeddc8d
-                  MATRIX: 527->1|637->16|665->18|803->130|817->136|887->186|938->211|952->217|1021->266|1171->389|1186->395|1227->415|1370->532|1398->539|1430->544
-                  LINES: 20->1|25->1|27->3|32->8|32->8|32->8|33->9|33->9|33->9|37->13|37->13|37->13|44->20|44->20|45->21
+                  HASH: d2712e3f5f01486d97e50dba93958b0294d81b88
+                  MATRIX: 527->1|637->16|665->18|803->130|817->136|887->186|938->211|952->217|1021->266|1388->606|1403->612|1444->632|1587->749|1615->756|1647->761
+                  LINES: 20->1|25->1|27->3|32->8|32->8|32->8|33->9|33->9|33->9|39->15|39->15|39->15|46->22|46->22|47->23
                   -- GENERATED --
               */
           
